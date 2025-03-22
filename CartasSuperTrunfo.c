161656d7313bba7e2d760c9a1087174afd00cc7d
rduque777 <rdcode777@gmail.com>
2025-03-22 05:48:26 +0000

    alteração e correção

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    int População;
-    float Área;
-    float PIB;
-    int Número de ponto turistico;
+    int populacao;
+    float area;
+    float pib;
+    int numero_de_pontos_turisticos;
 
-    scanf("Digite a População: \n");
-    printf("%d", &População);
+  
+    printf("Digite a população: \n");
+    scanf("%d", &populacao);
+  
+    printf("Digite a área: \n");
+    scanf("%f", &area);
+  
+    printf("Digite o PIB: \n");
+    scanf("%f", &pib);
 
-    scanf("Digite a Área: \n");
-    printf("%f", &Área);
-
-    scanf("Digite o PIB: \n");
-    printf("%f", &PIB);
-
-    scanf("Digite o Número de ponto turistico: \n");
-    printf("%F", &Número de ponto turistico);
-
+    printf("Digite o número de pontos turísticos: \n");
+    scanf("%d", &numero_de_pontos_turisticos);
+    
 
     return 0;
 }
